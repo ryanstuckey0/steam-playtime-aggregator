c9e3550b2c8de49898460a898deebbdb36c8a210
Ryan Stuckey <ryanstuckey0@gmail.com>
2025-01-09 21:16:53 -0500

    changed rest template to use parameterized type reference to prevent deserialization issues

--- src/main/java/com/stucko09/steam_aggregator/service/SteamApiService.java
+++ src/main/java/com/stucko09/steam_aggregator/service/SteamApiService.java
@@ -3,17 +3,22 @@ package com.stucko09.steam_aggregator.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stucko09.steam_aggregator.model.steam.SteamGetOwnedGamesResponse;
 import com.stucko09.steam_aggregator.model.steam.SteamResponse;
 
+import lombok.extern.apachecommons.CommonsLog;
+
+@CommonsLog
 @Service
 public class SteamApiService {
-    private static final String GET_OWNED_GAMES_REQUEST_PATH = "/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json";
+    private static final String GET_OWNED_GAMES_REQUEST_PATH = "/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json&include_appinfo=1";
 
     @Value("${steam.hostname}")
     private String steamApiHostname;
@@ -24,13 +29,15 @@ public class SteamApiService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public SteamResponse<SteamGetOwnedGamesResponse> getOwnedGames(String steamId) {
+    public SteamResponse<SteamGetOwnedGamesResponse> getOwnedGames(Long steamId) {
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get(steamApiHostname + String.format(GET_OWNED_GAMES_REQUEST_PATH, apiKey, steamId))
+                .accept(MediaType.APPLICATION_JSON).build();
         ResponseEntity<SteamResponse<SteamGetOwnedGamesResponse>> response = restTemplate.exchange(
-                steamApiHostname + String.format(GET_OWNED_GAMES_REQUEST_PATH, apiKey, steamId),
-                HttpMethod.GET,
-                null,
+                requestEntity,
                 new ParameterizedTypeReference<SteamResponse<SteamGetOwnedGamesResponse>>() {
                 });
+
         return response.getBody();
     }
 }
