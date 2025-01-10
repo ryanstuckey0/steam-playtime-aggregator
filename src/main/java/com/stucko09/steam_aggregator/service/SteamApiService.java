64916ad4b6835b536524bcae13f7bce9f0584ed2
Ryan Stuckey <ryanstuckey0@gmail.com>
2025-01-09 21:26:02 -0500

    moved Steam configuration to its own class will need to configure some secrets manager for API key

--- src/main/java/com/stucko09/steam_aggregator/service/SteamApiService.java
+++ src/main/java/com/stucko09/steam_aggregator/service/SteamApiService.java
@@ -1,7 +1,6 @@
 package com.stucko09.steam_aggregator.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -9,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stucko09.steam_aggregator.config.SteamProperties;
 import com.stucko09.steam_aggregator.model.steam.SteamGetOwnedGamesResponse;
 import com.stucko09.steam_aggregator.model.steam.SteamResponse;
 
@@ -20,18 +19,16 @@ import lombok.extern.apachecommons.CommonsLog;
 public class SteamApiService {
     private static final String GET_OWNED_GAMES_REQUEST_PATH = "/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json&include_appinfo=1";
 
-    @Value("${steam.hostname}")
-    private String steamApiHostname;
-
-    @Value("${steam.api-key}")
-    private String apiKey;
+    @Autowired
+    private SteamProperties steamProperties;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public SteamResponse<SteamGetOwnedGamesResponse> getOwnedGames(Long steamId) {
         RequestEntity<Void> requestEntity = RequestEntity
-                .get(steamApiHostname + String.format(GET_OWNED_GAMES_REQUEST_PATH, apiKey, steamId))
+                .get(steamProperties.getHostname()
+                        + String.format(GET_OWNED_GAMES_REQUEST_PATH, steamProperties.getApiKey(), steamId))
                 .accept(MediaType.APPLICATION_JSON).build();
         ResponseEntity<SteamResponse<SteamGetOwnedGamesResponse>> response = restTemplate.exchange(
                 requestEntity,
